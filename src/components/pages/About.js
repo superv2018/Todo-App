2af62e6f0106c0a06bc02d5e6f95473883a321dc
superv2018 <41586952+superv2018@users.noreply.github.com>
2019-08-18 22:02:11 +0300

    first todo-app v1.0.0

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>TodoList App v1.0.0 </p>
+        </React.Fragment>
+    )
+}
+
+export default About;
